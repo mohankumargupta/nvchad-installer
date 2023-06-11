ac58d7bbe7b811e44dfa6c002b37732e18f6a9e9
Mohan Gupta <mohangupta@live.com>
2023-06-11 17:57:09 +1000

    backing up config folder

--- install_gum.sh
+++ install_gum.sh
@@ -16,7 +16,7 @@ download_gum_binary() {
   source  "${temp_dir}/original_gum_script.sh"
   find_last_gum_release
   #wq
-  echo "$last_gum_release"
+  #echo "$last_gum_release"
   uname_os_check
   uname_arch_check
   platform_check
@@ -25,12 +25,12 @@ download_gum_binary() {
   github_artifact_url="https://github.com/charmbracelet/gum/releases/download/v${last_gum_release}/${latest_download_file_name}"
   #echo "$github_artifact_url"
   cd "$temp_dir"
-  curl -sSL "$github_artifact_url" > $latest_download_file_name
+  curl -sSL "$github_artifact_url" > "$latest_download_file_name"
   untar "$latest_download_file_name"
 }
 
 is_neovim_installed() {
-  command -v nvim
+  command -v nvim >/dev/null
   if [ $? -eq 1 ];
   then
     echo "Neovim not installed. Please install it first."
@@ -45,23 +45,74 @@ installed_neovim_version() {
   echo -e "${major}.${minor}.${patch}"
 }
 
-required_neovim_version() {
-  cd "$temp_dir"
-  curl -sSL https://raw.githubusercontent.com/raspberrypisig/semver-tool/master/src/semver > semver
-  source "$temp_dir/semver"
-  required_version=$(curl -sSL https://nvchad.com/docs/quickstart/install | grep /tag/v|sed -r 's/.*v([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
-  older=$(command_compare $1 $required_version)
-  if [ $older -eq -1 ];
+#required_neovim_version() {
+#  set -x
+#  cd "$temp_dir"
+#  curl -sSL https://raw.githubusercontent.com/raspberrypisig/semver-tool/master/src/semver > semver
+#  source "$temp_dir/semver"
+#  required_version=$(curl -sSL https://nvchad.com/docs/quickstart/install | grep /tag/v|sed -r 's/.*v([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+#  older=$(command_compare $1 $required_version)
+#  if [ $older -eq -1 ];
+#  then
+#    echo "Neovim version too old. Install new version of Neovim."
+#    exit 2
+#  fi
+#}
+
+backup_config_files() {
+  config_dir=$(nvim --clean --headless -c ':lua print(vim.fn.stdpath("config"))' -cq 3>&1 1>&2 2>&3)
+  data_dir=$(nvim --clean --headless -c ':lua print(vim.fn.stdpath("data"))' -cq 3>&1 1>&2 2>&3)
+  if [ -d "$config_dir" ];
   then
-    echo "Neovim version too old. Install new version of Neovim."
-    exit 2
+     echo -e "${config_dir} needs to be backed up and ${data_dir} will be removed. Continue?"
+     delete_dirs=$($gum choose "yes" "no")
+     if [ "$delete_dirs" == "yes" ];
+     then
+       echo Deleting...
+       timestamp_now=$(date +%F_%T|sed ''s/://g)
+       new_config_dir="${config_dir}-${timestamp_now}"
+       set -x
+       cp -r "$config_dir" "$new_config_dir"
+       rm -rf "${config_dir}"
+       rm -rf "${data_dir}"
+       set +x
+       echo Deleted.
+     else
+      echo "Wrong answer. Goodbye."
+      exit 1
+     fi
+  else
+    echo -e "Data directory ${data_dir} will be removed. Continue?"
+    delete_data_dirs=$($gum choose "yes" "no")
+    if [ "$delete_data_dirs" == "yes" ];
+    then
+      echo Deleting...
+       set -x
+       rm -rf "${data_dir}"
+       set +x
+       echo Deleted.
+    else
+      echo "Wrong answer. Goodbye."
+    fi
   fi
 }
 
-is_neovim_installed
-installed_version=$(installed_neovim_version)
-required_neovim_version $installed_version 
-download_gum_binary
+nvchad_installer() {
+  #installed_version=$(installed_neovim_version)
+  #required_neovim_version "$installed_version"
+  echo "Downloading gum binary to temp folder..."
+  download_gum_binary
+  clear
+  $gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "NVChad Installer Version $($gum style --foreground 212 'v0.0.1')."
+  $gum spin --spinner dot --title "Checking if neovim installed..." -- sleep 5
+  is_neovim_installed
+  backup_config_files
+}
 
-$gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "NVChad Installer Version $($gum style --foreground 212 'v0.0.1')."
+nvchad_installer
+
+
+
+
+
 
